bea86f00c3d2a8406a0f480d43b4f7daa13804ac
Saad <kakikebach1950@gmail.com>
2021-08-25 21:53:57 +0100

    I finalized the main menu here

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+	public GameObject options;
+
+	public GameObject menu;
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+	public void Play()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+	}
+
+	public void GoToOptions()
+	{
+		menu.SetActive(false);
+		options.SetActive(true);
+	}
+	public void GoToMenu()
+	{
+		menu.SetActive(true);
+		options.SetActive(false);
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			SceneManager.LoadScene(0);
+		}
+	}
+
+}

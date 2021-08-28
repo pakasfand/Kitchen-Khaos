b8156b16ea6e15861831431c81db275950e0f3f7
David <jdiazga@unal.edu.co>
2021-08-27 22:13:37 -0500

    Add Interaction Indicator above enemies

---
+++ Assets/Scripts/Dishes/InteractionIndicator.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class InteractionIndicator : MonoBehaviour
+{
+
+    MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public bool active
+    {
+        get
+        {
+            return _active;
+        }
+
+        set
+        {
+            _active = value;
+            meshRenderer.enabled = _active;
+        }
+
+    }
+
+    private bool _active;
+}

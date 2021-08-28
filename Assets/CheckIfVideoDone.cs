a818f8e3014bb3c7cd68932cb4aff34c3f902d54
Saad <kakikebach1950@gmail.com>
2021-08-28 12:02:58 +0100

    Implemented the cutscene and added a controls tab. You can skip the cutscene with enter

---
+++ Assets/CheckIfVideoDone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CheckIfVideoDone : MonoBehaviour
+{
+
+	public VideoPlayer video;
+
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			video.frame = (long)video.frameCount - 5;
+		}
+		
+		if(video.frame == (long)video.frameCount - 1)
+		{
+			print("Test");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+			
+		}
+	}
+}

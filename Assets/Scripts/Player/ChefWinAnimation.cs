437eac1edf30454c147ab9abf71e58583b9f7a6d
David <jdiazga@unal.edu.co>
2021-09-25 11:38:34 -0500

    Set up animations for winning and losing scenes.

---
+++ Assets/Scripts/Player/ChefWinAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChefWinAnimation : MonoBehaviour
+{
+    [SerializeField] Rigidbody rigidBody;
+    [SerializeField] float speed;
+    [SerializeField] AnimationClip run;
+    [SerializeField] AnimationClip flip;
+
+    float timer;
+
+    private void Start()
+    {
+        rigidBody.velocity = -Vector3.forward * speed;
+    }
+
+    public void Stop()
+    {
+        rigidBody.velocity = Vector3.zero;
+    }
+
+
+
+}
